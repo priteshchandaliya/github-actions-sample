5ccb069c77301f4878d8ea7346251d842ffd3f12
Pritesh Chandaliya <pritesh@aporeto.com>
2023-11-21 13:02:48 -0800

    adding app

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 provider "aws" {
   region = "us-east-1"  # Change this to your desired region
   shared_credentials_file = "/root/.aws/credentials"
-  version                 = "~> 3.0"  # Set the version explicitly
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu, change as needed
+  ami           = "ami-0230bd60aa48260c6"  # Ubuntu, change as needed
   instance_type = "t2.medium"  # Change to your desired instance type
 
   tags = {
