e3448d1729e021d19a390d254b9a65917f0f621f
Pritesh Chandaliya <pritesh@aporeto.com>
2023-11-21 13:35:30 -0800

    adding app

--- main.tf
+++ main.tf
@@ -6,18 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0230bd60aa48260c6"  # Ubuntu, change as needed
   instance_type = "t2.medium"  # Change to your desired instance type
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'CloudLabs12$' | sudo passwd ec2-user --stdin
-              echo 'PasswordAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
-              sudo systemctl restart sshd
-              sudo yum update -y
-              sudo amazon-linux-extras install docker -y
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker ec2-user
-              EOF
-
   tags = {
     Name = "nginx-instance"
   }
